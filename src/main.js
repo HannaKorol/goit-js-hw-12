fb9366e744a066fe3511127db8de543e59d66f56
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-08-13 06:32:07 +0200

    edited "Load more" button
    Приховування кнопки Load more та виведення сповіщення одночасно з рендером крайньої порції зображень.

--- src/main.js
+++ src/main.js
@@ -16,16 +16,18 @@ const loadMoreBtn = document.querySelector('[data-action="load-more"]');
 
 // 2) Користувач буде вводити рядок для пошуку в текстове поле, а за сабмітом форми необхідно виконувати HTTP-запит із цим пошуковим рядком.
 const form = document.querySelector(".form-search");
-let query = '';
-let page = 1;
-let maxPages = 0;
+let query = ''; // Змінна для зберігання запиту пошуку
+let page = 1; // Поточна сторінка
+let maxHits = 0; // Змінна для зберігання загальної кількості знайдених зображень
+let totalHits = 0; // Змінна для відстеження кількості вже відображених зображень
 
-
+// Обробник події при сабміті форми
 form.addEventListener("submit", async event => {
     event.preventDefault();
     //Обробка тексту що було введено користувачем в форму :
-query = event.currentTarget.elements.query.value.trim();
-page = 1;
+query = event.currentTarget.elements.query.value.trim(); // Отримання значення з інпуту
+page = 1; // Повернення на першу сторінку
+totalHits = 0;  // Скидання лічильника відображених зображень                                              // Reset the total hits counter
 
     // 3) При натисканні на кнопку відправки форми, додайте перевірку вмісту текстового поля на наявність порожнього рядка, щоб користувач не міг відправити запит, якщо поле пошуку порожнє.
     if(!query) {                           //якщо немає тексту в input то:
@@ -37,13 +39,13 @@ page = 1;
     }
 
 
-clearGallery();                                   
-showLoader();
-loadMoreBtn.classList.add('hidden');
+clearGallery();        // Очищення галереї                            
+showLoader();         // Показати індикатор завантаження
+loadMoreBtn.classList.add('hidden');  // Приховати кнопку "Load more"
 
 try {
   const data = await fetchImages(query, page);
-maxPages = data.maxpages;
+  maxHits = data.totalHits;  // Збереження загальної кількісті знайдених зображень
 
 
   if(data.hits.length === 0) { //Якщо масив hits порожній, відображається повідомлення про те, що зображень не знайдено.
@@ -53,22 +55,23 @@ message:
 position: 'topRight',
         });
     } else {
-        renderImages(data.hits); // Якщо ж зображення знайдено, вони рендеряться за допомогою функції renderImages.
+      totalHits += data.hits.length; // Оновлення кількісті відображених зображень
+        renderImages(data.hits); // Відображення зображення
 
-    if (data.hits.length < 15 || maxPages <= 15) {  // якщо картинок менше 15 то виводимо повідомлення що їх більше немає
+    if (totalHits >= maxHits) {  // Якщо всі зображення відображено
       iziToast.info({
 message: "We're sorry, but you've reached the end of search results.",
 position: 'topRight',
 });
 } else {
-  loadMoreBtn.classList.remove('hidden');  // ховаємо кнопку так як картиннок для відобрадення більше немає
+  loadMoreBtn.classList.remove('hidden');  // Показуємо кнопку "Load more", якщо є ще зображення
 }
       }
 } catch(error) {
   iziToast.error({ title: 'Error', message: error.message }); //якщо виникають помилки запиту - відображаємо за допомогою бібліотеки
 } finally {
-  hideLoader();                                               // під кінець ховаємо "загрузчика"
-  form.reset();                                               //очищаємо форму після зробленого запиту користувачем
+  hideLoader();                                               // Приховуємо індикатор завантаження
+  form.reset();                                               // Очистити форму після запиту
 }
 });
 
@@ -90,31 +93,34 @@ position: 'topRight',
 5. після запиту робимо перевірку: якщо поточна сторінка співпадає з максимальною - то ховаємо кнопку і знімаємо слухача подій, якщо ж все ок - розблоковуємо кнопку для подальшої взаємодії
 
 */
-
+// Обробник події при натисканні кнопки "Load more"
 loadMoreBtn.addEventListener("click", async () =>{         //якщо натиснули на Кнопку "завантажити ще" то відображається ще одна нова сторінка
-  page+=1;
-  showLoader();
+  page+=1;                                                 // Перехід на наступну сторінку
+  showLoader();                                            // Показування індикатора завантаження
 
   try{
-    const data = await fetchImages(query, page);              
-    if (data.hits.length === 0 || page * 15 >= maxPages) {
+    const data = await fetchImages(query, page);
+    totalHits += data.hits.length;                          // Оновлення кількісті відображених зображень      
+    renderImages(data.hits);                                // Відображення нових зображень
+    smoothScroll();                                         // Плавне прокручування до нових зображень
+
+    if (totalHits >= maxHits) {                              // Якщо всі зображення відображено
       iziToast.info({
         message: "We're sorry, but you've reached the end of search results.",
         position: 'topRight',
       });
-      loadMoreBtn.classList.add('hidden');
+      loadMoreBtn.classList.add('hidden');                   // Приховати кнопку "Load more"
     }
-  renderImages(data.hits);
-  smoothScroll();
+
   } catch(error) {
     iziToast.error({ title: 'Error', message: error.message });
   } finally {
-    hideLoader();
+    hideLoader();                                                 // Приховати індикатор завантаження
   }
 });
 
 
-
+// Функція для плавного прокручування до нових зображень
 function smoothScroll() {
   const { height: cardHeight } = document
   .querySelector('.gallery-list')
